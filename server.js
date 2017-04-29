4477410c8ad72304582e8d768a7d0d306327363d
Ranger Alone <Ranger Alone>
2017-04-29 11:40:59 +0800

    add env arguments

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 let express = require('express');
 let mongo = require('mongodb');
 let https = require('https');
-const dbUrl = 'mongodb://milesxu:hM73DseS17pu@ds062339.mlab.com:62339/mongo'
-const key1 = '7c58b30f8f7a4a6b930759332af433ed';
-const key2 = 'df62649f932445b2a7ae8fbe93b652ec';
+const dbUrl = `mongodb://${process.env.dbuser}:${process.env.dbpass}@ds062339.mlab.com:62339/mongo`
 let options = {
     hostname: 'api.cognitive.microsoft.com',
     path: '/bing/v5.0/images/search',
     headers: {
-        'Ocp-Apim-Subscription-Key': Math.random() > 0.5 ? key1 : key2
+        'Ocp-Apim-Subscription-Key': Math.random() > 0.5 ?
+            process.env.key1 : process.env.key2
     },
     agent: false
 }
@@ -59,7 +58,9 @@ function getSearchResult(imageList) {
 let app = express();
 app.get('/', (req, res) => {
     //res.sendFile(__dirname + '/index.html');
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', {
+        root: __dirname
+    });
 });
 app.get('/api/imagesearch/:searchStr', (req, res) => {
     createSearchString(req.params.searchStr, req.query.offset);
